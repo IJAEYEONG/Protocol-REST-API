e59c7048a3ffbc10b094bf71d320cd0199a47683
test <leejeayoung875@gmail.com>
2024-06-14 14:54:27 +0900

    server.js 생성

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
 
-        const BaseUrl =`https://api.themoviedb.org/3/movie/popular`;
-        const Api_key=`10ad75c569818326a7686e6984c4d996`;
-        const language=`ko-kr`;
-        const startPate=1;
-        const maxPage=500;
-        const root =document.getElementById('root')
+const BaseUrl = 'https://api.themoviedb.org/3/movie/popular';
+const Api_key = '10ad75c569818326a7686e6984c4d996';
+const language = 'ko-kr';
+const maxPage = 500;
 
-        function test(page){
-          const xhr =new XMLHttpRequest();
-          const reqMethod ="GET";
-          const requestUrl =`${BaseUrl}?api_key=${Api_key}&language=${language}&page=${page}`
-          console.log(requestUrl)
-          xhr.open(reqMethod,requestUrl,true)
-          xhr.addEventListener('load',()=>{
-            if(xhr.status===200){
-              const result =JSON.parse(xhr.responseText)
-              let list =[];
-              result.results.forEach(element => {
-                list.push(`<li><img src="https://image.tmdb.org/t/p/w500/${element.poster_path}}"><br>제목:${element.title}<br>내용:${element.overview}</li>`)
-              });
-              root.innerHTML+=`<ul>${list.join('')}</ul>`
-              }if(page<maxPage){
-                test(page+1)
-                console.log(page)
-              }else{
-                console.error('에러')
-              }
-            });
-            xhr.send();
-          }
-        test(startPate)
+app.get('/movies', async (req, res) => {
+    try {
+        const page = req.query.page || 1;
+        const requestUrl = `${BaseUrl}?api_key=${Api_key}&language=${language}&page=${page}`;
+        const response = await axios.get(requestUrl);
+        const result = response.data;
+
+        let list = [];
+        result.results.forEach(element => {
+            list.push(`<li><img src="https://image.tmdb.org/t/p/w500/${element.poster_path}"><br>제목:${element.title}<br>내용:${element.overview}</li>`);
+        });
+
+        res.send(`<ul>${list.join('')}</ul>`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('에러 발생');
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
